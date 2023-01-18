7d13209bc6a47cd20000dc7a59a5b4ec9c750420
Angelo Arevalo Laurente <soyangelo17.2002@gmail.com>
2023-01-17 23:53:19 -0500

    validando errores de formulario

--- assets/js/fromulario.js
+++ assets/js/fromulario.js
@@ -1,12 +1,13 @@
 // Variables
 const btnEnviar = document.querySelector('#enviar');
-
+const formulario = document.querySelector('#enviar-mail')
 
 // Variables para campos
 const nombre = document.querySelector('#nombre');
 const numCelular = document.querySelector('#telefono');
 const fecha = document.querySelector('#date');
 const mensaje = document.querySelector('#mensaje');
+const suscribete = document.querySelector('#suscribete');
 
 
 
@@ -23,6 +24,8 @@ function eventListeners() {
     numCelular.addEventListener('blur', validarFormulario )
     fecha.addEventListener('blur', validarFormulario )
     mensaje.addEventListener('blur', validarFormulario )
+    suscribete.addEventListener('blur', validarFormulario )
+    
 }
 
 
@@ -30,15 +33,33 @@ function eventListeners() {
 //Funciones
 function iniciarApp() {
     btnEnviar.disabled = true;
-    btnEnviar.classList.add('active');
+    btnEnviar.classList.add('active');  //para cuando arranqque la web
+                                        //aparezca oscuro el boton y no poder presionarlo
 }
 
 
-// Valida formulario
+// Valida formulario             marca los bordes de rojo cuan
 function validarFormulario(e) {
     if(e.target.value.length > 0) {
         console.log('hay algo')
     } else {
        e.target.classList.add('active')
+
+       mostrarError();
     }
+
+
+}
+
+//Error de formulario
+function mostrarError() {
+    const mensajeError = document.querySelector('#enviar-mail')
+    mensajeError.classList.add('active');
+
+    //para que no se repita cada vez q no llenemos nd
+    const errores = document.querySelectorAll('.error');
+    if(errores.length === 0) {
+        formulario.appendChild(mensajeError);
+    }
+   
 }
